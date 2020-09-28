f1e9c65ce24bb5627f56fd804bd75de0cd999bc4
SIDDARAJUSIDDASETTY <31282090+SIDDARAJUSIDDASETTY@users.noreply.github.com>
2020-09-28 17:51:06 +0530

    Add files via upload

---
+++ build/precache-manifest.e17b4e10c7beb1f3bf558cc3eb41bd1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7969784f9c30b1bb14c942d966dc632",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d6d27b73f2ffab6a49b",
+    "url": "/static/css/main.b48f20f7.chunk.css"
+  },
+  {
+    "revision": "a7030578b9f477d2d465",
+    "url": "/static/js/2.12bcec45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.12bcec45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d6d27b73f2ffab6a49b",
+    "url": "/static/js/main.97b59827.chunk.js"
+  },
+  {
+    "revision": "4af822c69337c4235627",
+    "url": "/static/js/runtime-main.ed5bfbd4.js"
+  }
+]);
---
+++ src/quizService/index.js
@@ -0,0 +1,365 @@
+const qBank = [
+  {
+    question:
+      "Which of the following countries has agreed to accept the payment of export of oil and petroleum products to India, in rupee terms instead of dollar or any other currency?",
+    answers: ["Srilanka", "Iran", "Japan", "Austria"],
+    correct: "Iran",
+    questionId: "099099"
+  },
+  {
+    question:
+      "As per the newspaper reports Government of India has finally agreed to purchase advanced MRMRs for its naval forces. What are these MRMRs?",
+    answers: ["Rocket Launcher", "Submarines", "Warships", "Aircrafts"],
+    correct: "Aircrafts",
+    questionId: "183452"
+  },
+  {
+    question:
+      "Who has started the Murtidevi Literature Award?",
+    answers: ["Ministry of Human Resource Development", "Bharatiya Jnanpith Trust", "Sahitya Academy", "Bharatiya Vidya Bhavan"],
+    correct: "Bharatiya Jnanpith Trust",
+    questionId: "267908"
+  },
+  {
+    question: "The Sharda Act was related to?",
+    answers: [
+      "Indian Literature",
+      "Indian marriage arrangement",
+      "Indian Economy",
+      "Indian Administration"
+    ],
+    correct: "Indian marriage arrangement",
+    questionId: "333247"
+  },
+  {
+    question: "When was the Indian Army formed?",
+    answers: ["1747", "1847", "1749", "1748"],
+    correct: "1847",
+    questionId: "496293"
+  },
+  {
+    question:
+      "When was Delhi's first newspaper published?",
+    answers: ["1836","1830","1827","1825"],
+    correct: "1836",
+    questionId: "588909"
+  },
+  {
+    question:
+      'In which place is Delhi in greenery in the country?',
+    answers: ["First", "Second", "Third", "Fourth"],
+    correct: "Fourth",
+    questionId: "648452"
+  },
+  {
+    question: "What is the first book of the Old Testament?",
+    answers: ["Genesis", "Exodus", "Leviticus", "Numbers"],
+    correct: "Genesis",
+    questionId: "786649"
+  },
+  {
+    question:
+      "In the video-game franchise Kingdom Hearts, the main protagonist, carries a weapon with what shape?",
+    answers: ["Key", "Sword", "Pen", "Cellphone"],
+    correct: "Key",
+    questionId: "839754"
+  },
+  {
+    question:
+      "In which state is the Rihand Dam?",
+    answers: [
+      "Punjab",
+      "Uttar Pradesh",
+      "Karnataka",
+      "Tamil Nadu"
+    ],
+    correct: "Uttar Pradesh",
+    questionId: "98390"
+  },
+  {
+    question: " Who is called the savior of India and the father of local self-government?",
+    answers: ["Lord Henstin", "Lord Patrick", "Lord Ripon", "Lord Litton"],
+    correct: "Lord Litton",
+    questionId: "1071006"
+  },
+  {
+    question: "In past times, what would a gentleman keep in his fob pocket?",
+    answers: ["Watch", "Money", "Keys", "Notebook"],
+    correct: "Watch",
+    questionId: "1174154"
+  },
+  {
+    question: "Which of the following river passes through Vibransh valley in the following?",
+    answers: ["Krishna", "Godavari", "Vyas", "Tapti"],
+    correct: "Godavari",
+    questionId: "1226535"
+  },
+  {
+    question: "Which Mughal ruler built the Red Fort in Delhi?",
+    answers: ["Shahjahan", "Akbar", "Jahangir", "Aurangzeb"],
+    correct: "Shahjahan",
+    questionId: "1310938"
+  },
+  {
+    question: "What is the largest organ of the human body?",
+    answers: ["Skin", "Heart", "large Intestine", "Liver"],
+    correct: "Skin",
+    questionId: "1436873"
+  },
+  {
+    question: "Which sign of the zodiac is represented by the Crab?",
+    answers: ["Cancer", "Libra", "Virgo", "Sagittarius"],
+    correct: "Cancer",
+    questionId: "1515110"
+  },
+  {
+    question: "On a dartboard, what number is directly opposite No. 1?",
+    answers: ["19", "20", "12", "15"],
+    correct: "19",
+    questionId: "1642728"
+  },
+  {
+    question:
+      "What does the 'S' stand for in the abbreviation SIM, as in SIM card? ",
+    answers: ["Subscriber", "Single", "Secure", "Solid"],
+    correct: "Subscriber",
+    questionId: "1747256"
+  },
+  {
+    question:
+      "District information is available?",
+    answers: ["Monasteries", "Economics", "Buddhist Literature", "Mehrauli iron pillar"],
+    correct: "Buddhist Literature",
+    questionId: "1822532"
+  },
+  {
+    question: "Which American president appears on a one dollar bill?",
+    answers: [
+      "George Washington",
+      "Thomas Jefferson",
+      "Abraham Lincoln",
+      "Benjamin Franklin"
+    ],
+    correct: "George Washington",
+    questionId: "195075"
+  },
+  {
+    question: 'What is "dabbing"?',
+    answers: ["A dance", "A medical procedure", "A sport", "A language"],
+    correct: "A dance",
+    questionId: "2019778"
+  },
+  {
+    question: "Approximately what percentage of the moon appears at the time of super moon?",
+    answers: ["5%", "14%", "50%", "20%"],
+    correct: "14%",
+    questionId: "2134343"
+  },
+  {
+    question: "Which one of the following planets takes the maximum time for an orbit around the Sun?",
+    answers: [
+      "Earth",
+      "Mars",
+      "Jupiter",
+      "Venus"
+    ],
+    correct: "Jupiter",
+    questionId: "2210799"
+  },
+  {
+    question:
+      "Which of the following is the birthplace of Buddha?",
+    answers: ["Bodh Gaya", "Sarnath", "Lumbini", "Varanasi"],
+    correct: "Lumbini",
+    questionId: "2379831"
+  },
+  {
+    question: "Who directed India's first publicly screened cinema Raja Harishchandra?",
+    answers: ["V Shantaram", "V G Damle", "Dada Saheb Phalke", "Bimal Roy"],
+    correct: "Dada Saheb Phalke",
+    questionId: "2426418"
+  },
+  {
+    question: "What is the nickname of the US state of California?",
+    answers: ["Golden State", "Sunshine State", "Bay State", "Treasure State"],
+    correct: "Golden State",
+    questionId: "2510086"
+  },
+  {
+    question: " Amoghavarsha was related to which dynasty?",
+    answers: [
+      "Rashtrakuta",
+      "Chola",
+      "Cher",
+      "Pandya"
+    ],
+    correct: "Rashtrakuta",
+    questionId: "2685745"
+  },
+  {
+    question:
+      "Where is our national symbol taken from?",
+    answers: ["From Buddhism located in Gaya", "Stupa of Sanchi", "From the Ashoka pillar situated in sarnath", "From the remains found in Sindhu ghati"],
+    correct: "From the Ashoka pillar situated in Sarnath",
+    questionId: "2796884"
+  },
+  {
+    question: "Which of these colours is NOT featured in the logo for Google?",
+    answers: ["Pink", "Yellow", "Blue", "Green"],
+    correct: "Pink",
+    questionId: "2838900"
+  },
+  {
+    question: 'Which country has launched a satellite named Ravana-1 in space?',
+    answers: ["Nepal", "Pakishtan", "India", " Sri Lanka"],
+    correct: "Sri Lanka",
+    questionId: "298060"
+  },
+  {
+    question: "Who is the Grand Old Man of India?",
+    answers: ["Dadabhai Naoroji", "Mahatma Gandhi", "Gopalakrishna Gokhale", "Subhash Chandra Bose"],
+    correct: "Dadavhai Naoroji",
+    questionId: "3096579"
+  },
+  {
+    question: "When did the Swachh Bharat Abhiyan start?",
+    answers: [
+      "1 April 2015",
+      "9 December 2014",
+      "2 October 2014",
+      "26 January 2015"
+    ],
+    correct: "2 October 2014",
+    questionId: "3182461"
+  },
+  {
+    question: "What do the letters in the GMT time zone stand for?",
+    answers: [
+      "Greenwich Mean Time",
+      "Global Meridian Time",
+      "General Median Time",
+      "Glasgow Man Time"
+    ],
+    correct: "Greenwich Mean Time",
+    questionId: "3239112"
+  },
+  {
+    question: "Who was the Governor General during the independence of India?",
+    answers: ["Lord Dalhousie", "Lord Cripps", "Lord Curzon", "Lord Mountbatten"],
+    correct: "Lord Mountbatten",
+    questionId: "3318503"
+  },
+  {
+    question:
+      ' When was the Nirmal Bharat Abhiyan formed?',
+    answers: ["1995", "1999", "1998", "2005"],
+    correct: "1999",
+    questionId: "3410327"
+  },
+  {
+    question: "Who was the first President of All India Trade Union Congress?",
+    answers: ["Deewana Chamanlal", "Lala Rajpat Rai", "Swami Sahajanand", "Jawaharlal Nehru"],
+    correct: "Lala Rajpat Rai",
+    questionId: "3542596"
+  },
+  {
+    question: " When was the Bharatiya Jan Sangh founded?",
+    answers: ["27 Sep 1925", "25 March 1960", "21 October 1951", "Others"],
+    correct: "21 October 1951",
+    questionId: "3667517"
+  },
+  {
+    question: "What do the letters of the fast food chain KFC stand for?",
+    answers: [
+      "Kentucky Fried Chicken",
+      "Kentucky Fresh Cheese",
+      "Kibbled Freaky Cow",
+      "Kiwi Food Cut"
+    ],
+    correct: "Kentucky Fried Chicken",
+    questionId: "3791672"
+  },
+  {
+    question: "India's first Minister of Law and Justice?",
+    answers: ["Ashok Kumar Sen", "Hans Raj Khanna", "B R Ambedkar", "Others"],
+    correct: "B R Ambedkar",
+    questionId: "3893585"
+  },
+  {
+    question: 'What color is the "Ex" in FedEx Ground?',
+    answers: ["Green", "Red", "Light Blue", "Orange"],
+    correct: "Green",
+    questionId: "3913430"
+  },
+  {
+    question: "How tall is the Burj Khalifa?",
+    answers: ["2,722 ft", "2,717 ft", "2,546 ft", "3,024 ft"],
+    correct: "2,722 ft",
+    questionId: "4049121"
+  },
+  {
+    question:
+      "What is the minimum age to be a Lok Sabha candidate?",
+    answers: ["25", "30", "35", "18"],
+    correct: "25",
+    questionId: "4150746"
+  },
+  {
+    question:
+      "Where is the headquarters of Bharatiya Janata Party?",
+    answers: ["Karnataka", "Jharkhand", "Gujarat", "New Delhi"],
+    correct: "New Delhi",
+    questionId: "4235063"
+  },
+  {
+    question: "Which sign of the zodiac comes between Virgo and Scorpio?",
+    answers: ["Libra", "Gemini", "Taurus", "Capricorn"],
+    correct: "Libra",
+    questionId: "4321002"
+  },
+  {
+    question: "What is the minimum age to be a Rajya Sabha candidate?",
+    answers: ["20","30","45","18"],
+    correct: "20",
+    questionId: "4442286"
+  },
+  {
+    question: "Where is the headquarters of Rashtriya Swayamsevak Sangh?",
+    answers: [
+      "Nagpur",
+      "Bhopal",
+      "Agra",
+      "Ahmedabad"
+    ],
+    correct: "Nagpur",
+    questionId: "4564430"
+  },
+  {
+    question: "“Vijayi vishw Tiranga Pyara” slogan was given by whom?",
+    answers: ["Dayanand Saraswati", "Ravindranath Tagore", "Mangal Pandey", "Shyamlal Gupta Parshad"],
+    correct: "Shyamlal Gupta Parshad",
+    questionId: "4630606"
+  },
+  {
+    question:
+      "Who among the following was the editor of “Bharat Mata” magazine?",
+    answers: ["Shacheendranath Sanyal", "Arvind Ghosh", "Ajeet singh", "Bhagvati Charan Bohra"],
+    correct: "Ajeet singh",
+    questionId: "4795960"
+  },
+  {
+    question: "Which candy is NOT made by Mars?",
+    answers: ["Almond Joy", "M&M's", "Twix", "Snickers"],
+    correct: "Almond Joy",
+    questionId: "4811162"
+  },
+  {
+    question: "Who is the Highest Law Officer in India?",
+    answers: ["Secretary general", "Solicitor general", "Attorney general", "Advocate general"],
+    correct: "Attorney general",
+    questionId: "4982981"
+  }
+];
+
+export default (n = 5) =>
+  Promise.resolve(qBank.sort(() => 0.5 - Math.random()).slice(0, n));
